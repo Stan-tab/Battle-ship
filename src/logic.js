43807cd7e35a6a1d9668c4008f32de8e7f3ff682
Stan-tab <godofwarmafia2@gmail.com>
2025-04-12 12:33:51 +0500

    Add positioning of ships

--- src/logic.js
+++ src/logic.js
@@ -5,4 +5,43 @@ function ship(length) {
 	this.isSunk = () => this.length <= this.hits;
 }
 
-module.exports = { ship };
+function gameBoard() {
+	this.grid = [
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	];
+
+	this.placeShip = (arr, ship, direction = false) => {
+		const [x, y] = this.orientShip(arr, ship.length, direction);
+
+		if (direction) {
+			for (let i = 0; i < ship.length; i++) this.grid[y + i][x] = ship;
+			return;
+		}
+		for (let i = 0; i < ship.length; i++) {
+			this.grid[y][x + i] = ship;
+		}
+	};
+	this.orientShip = (arr, length, direction = false) => {
+		const [x, y] = arr;
+		const cursor = Math.floor(length / 2);
+		let orient = x;
+		if (direction) orient = y;
+
+		let begin = orient - length + cursor;
+		if (begin < 0) begin = 0;
+		if (begin + length > 10) begin = 10 - length;
+
+		return !direction ? [begin, y] : [x, begin];
+	};
+}
+
+module.exports = { ship, gameBoard };
--- test/logic.test.js
+++ test/logic.test.js
@@ -11,3 +11,34 @@ test('Testing ship factory', () => {
 	expect(ship.hits).toBe(3);
 	expect(ship.isSunk()).toBeTruthy();
 });
+
+test('test gameboard', () => {
+	const ship = new logic.ship(4);
+	const board = new logic.gameBoard();
+	board.placeShip([0, 0], ship);
+	expect(board.grid).toStrictEqual([
+		[ship, ship, ship, ship, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	]);
+	board.placeShip([3, 4], ship);
+	expect(board.grid).toStrictEqual([
+		[ship, ship, ship, ship, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, ship, ship, ship, ship, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	]);
+});
